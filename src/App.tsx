359f854925021701a49d2f93506a56a9a220aa38
UlrikeWerner <ulrike_werner@web.de>
2023-10-02 11:12:08 +0200

    add axios

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,29 @@ import TopNavigation from "./TopNavigation.tsx";
 import {Route, Routes} from "react-router-dom";
 import HomePage from "./HomePage.tsx";
 import CharacterGallery from "./CharacterGallery.tsx";
-import {Character, charactersResponse} from "./characters.ts";
+import {Character} from "./characters.ts";
 import CharacterDetails from "./CharacterDetails.tsx";
 
 import AddCharacter from "./AddCharacter.tsx";
+import axios from "axios";
 
 export default function App() {
-    const [characters, setCharacters] = useState<Character[]>(charactersResponse.results);
+    const [characters, setCharacters] = useState<Character[]>([]);
 
     useEffect(() => {
-        console.log("test");
+        loadCharacters();
     }, []);
 
+    function loadCharacters() {
+        axios.get("https://rickandmortyapi.com/api/character")
+            .then((response) => {
+                setCharacters(response.data.reuslts);
+            })
+            .catch((reason) => {
+                console.error("Die Daten konnten nicht angezeigt werden");
+                console.error(reason.status);
+            })
+    }
 
     function saveCharacter(characterToSave: Character) {
         setCharacters([...characters, characterToSave])
